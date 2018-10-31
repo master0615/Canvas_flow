f092c3240d35f4920e71e99aff184c95ec466c94
Pavel Panov <paveltiger9@gmail.com>
2018-11-01 01:37:57 +1000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxGraphModule } from './ngx-graph';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule, 
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgbModule,
+    NgxChartsModule,
+    NgxGraphModule
   ],
   providers: [],
   bootstrap: [AppComponent]
